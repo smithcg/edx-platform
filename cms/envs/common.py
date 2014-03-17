8dc6111e23f1eb60697efa9f91a29a54356e29a5
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-03-17 16:45:15 +0100

    Add XForwardedFor middleware

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -194,6 +194,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'method_override.middleware.MethodOverrideMiddleware',
 
+    # Reverse proxy
+    'x_forwarded_for.middleware.XForwardedForMiddleware',
+
     # Instead of AuthenticationMiddleware, we use a cache-backed version
     'cache_toolbox.middleware.CacheBackedAuthenticationMiddleware',
     'student.middleware.UserStandingMiddleware',
@@ -527,6 +530,9 @@ INSTALLED_APPS = (
 
     # Monitoring signals
     'monitoring',
+
+    # Reverse proxy
+    'x_forwarded_for',
 )
 
 
--- lms/envs/common.py
+++ lms/envs/common.py
@@ -766,6 +766,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 
+    # Reverse proxy
+    'x_forwarded_for.middleware.XForwardedForMiddleware',
+
     # Instead of AuthenticationMiddleware, we use a cached backed version
     #'django.contrib.auth.middleware.AuthenticationMiddleware',
     'cache_toolbox.middleware.CacheBackedAuthenticationMiddleware',
@@ -1265,6 +1268,9 @@ INSTALLED_APPS = (
 
     # Monitoring functionality
     'monitoring',
+
+    # Reverse proxy
+    'x_forwarded_for',
 )
 
 ######################### MARKETING SITE ###############################
